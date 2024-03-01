2a1427c638fb7d6f8b3dad7be564220c39411735
Mawj_Aljadirjy <eng_mawj_aljadirjy@yahoo.com>
2024-03-01 18:29:03 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+
+const  cats= ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat=(name)=>{
+    cats.push(name)
+
+}
+
+
+const destructivelyPrependCat=(name)=>{
+    cats.unshift(name)
+
+}
+
+destructivelyRemoveLastCat=()=>{
+    cats.pop()
+}
+
+destructivelyRemoveFirstCat=()=>{
+    cats.shift()
+}
+
+const appendCat = (catName) => {
+    const newArray = [...cats]; 
+    newArray.push(catName);
+    return newArray;
+}
+
+const prependCat = (catName) => {
+    const newArray = [...cats]; 
+    newArray.unshift(catName);
+    return newArray;
+}
+
+const removeLastCat = (catName) => {
+    const newArray = [...cats]; 
+    newArray.pop(catName);
+    return newArray;
+}
+
+const removeFirstCat = (catName) => {
+    const newArray = [...cats]; 
+    newArray.shift(catName);
+    return newArray;
+}
